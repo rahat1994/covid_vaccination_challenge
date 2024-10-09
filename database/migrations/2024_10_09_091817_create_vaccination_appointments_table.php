5a55ce38d8a2ac365dcd0ac5daf97737571ddcd5
Rahat Baksh <rahat392@gmail.com>
2024-10-09 15:30:20 +0600

    Add migration files for vaccination centers, appointments, and user modifications

---
+++ database/migrations/2024_10_09_091817_create_vaccination_appointments_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vaccination_appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vaccination_center_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->dateTime('appointment_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vaccination_appointments');
+    }
+};
