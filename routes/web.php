8578aef271d202f90571d3d752e1c05b31934502
Rahat Baksh <rahat392@gmail.com>
2024-10-10 18:34:36 +0600

    implementing the search by nid

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,25 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::get('/', function (Request $request) {
+    return Inertia::render('Welcome');
 });
+
+Route::post('get-status', function (Request $request) {
+    $request->validate([
+        'value' => ['required', 'string', 'regex:/^\d{13}$|^\d{17}$/'],
+    ]);
+    $user = User::whereNid($request->value)->first();
+    return Inertia::render('Welcome', [
+        'user' => $user
+    ]);
+    
+})->name('get-status');
 
 Route::middleware([
     'auth:sanctum',
