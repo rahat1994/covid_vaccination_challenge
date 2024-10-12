76b5cd7d9b32533bbeacbb110e8f896552922556
Rahat Baksh <rahat392@gmail.com>
2024-10-12 18:33:47 +0600

    updating registration test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,11 +29,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'nid' => fake()->regexify('^\d{13}$|^\d{17}$'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
-            'remember_token' => Str::random(10),
+            'remember_token' => 'password',
             'profile_photo_path' => null,
             'current_team_id' => null,
         ];
@@ -44,7 +45,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
@@ -60,8 +61,8 @@ class UserFactory extends Factory
 
         return $this->has(
             Team::factory()
-                ->state(fn (array $attributes, User $user) => [
-                    'name' => $user->name.'\'s Team',
+                ->state(fn(array $attributes, User $user) => [
+                    'name' => $user->name . '\'s Team',
                     'user_id' => $user->id,
                     'personal_team' => true,
                 ])
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\VaccineCenter;
+use Database\Factories\UserFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia;
 use Laravel\Fortify\Features;
 use Laravel\Jetstream\Jetstream;
 use Tests\TestCase;
@@ -42,18 +44,20 @@ class RegistrationTest extends TestCase
 
         $vaccineCenter = VaccineCenter::factory()->create();
 
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'nid' => '1234567890123',
-            'email' => 'test@gmail.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-            'vaccine_center_id' => $vaccineCenter->id,
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
+        $response = $this->followingRedirects()
+            ->post('/register', [
+                'name' => 'Test User',
+                'nid' => '1234567890123',
+                'email' => 'test@gmail.com',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+                'vaccine_center_id' => $vaccineCenter->id,
+                'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+            ])->assertOk()
+            ->assertInertia(
+                fn(AssertableInertia $page) => $page
+                    ->component('Welcome')
+            );
     }
 
     public function test_new_users_can_not_register_with_invalid_nid(): void
@@ -64,41 +68,36 @@ class RegistrationTest extends TestCase
 
         $vaccineCenter = VaccineCenter::factory()->create();
 
-        $response = $this->post('/register', [
+        $this->post('/register', [
             'name' => 'Test User',
-            'nid' => 'invalid-nid',
             'email' => 'test@gmail.com',
+            'nid' => 'invalid-nid',
             'password' => 'password',
             'password_confirmation' => 'password',
             'vaccine_center_id' => $vaccineCenter->id,
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
-
+        ])->assertSessionHasErrors('nid');
     }
 
-    public function test_new_users_can_not_register_with_existing_nid(): void
+    public function test_new_users_can_not_rester_with_existing_nid()
     {
         if (! Features::enabled(Features::registration())) {
             $this->markTestSkipped('Registration support is not enabled.');
         }
 
         $vaccineCenter = VaccineCenter::factory()->create();
+        $user = UserFactory::new()->create([
+            'vaccination_center_id' => $vaccineCenter->id,
+        ]);
 
-        $response = $this->post('/register', [
+        $this->post('/register', [
             'name' => 'Test User',
-            'nid' => 'invalid-nid',
-            'email' => 'test@gmail.com',
+            'email' => fake()->unique()->safeEmail(),
+            'nid' => $user->nid,
             'password' => 'password',
             'password_confirmation' => 'password',
             'vaccine_center_id' => $vaccineCenter->id,
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
-
+        ])->assertSessionHasErrors('nid');
     }
 }
